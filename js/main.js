7b882cb34dbc25e5da64cb2108b182f410eb9a6b
Tanner-LeeT <tannertarkowski@gmail.com>
2024-03-22 17:07:36 -0600

    Added removal of favorites

--- js/main.js
+++ js/main.js
@@ -25,43 +25,51 @@ Steps 1-3 READ THE PDF!
     renderFavouriteList();
   });
 
-  function addToFavouriteGame(game) {
-    myGames.push(game);
-    console.log(myGames);
-  }
   // event listener for step 3
   myGameList.addEventListener('click', (event) => {
     let favGame = event.target.innerText;
+    removeFromFavouriteGames(favGame);
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    let videoGameForm = document.getElementById('video-game-form');
+    let allGameListItems = document.querySelectorAll('.all-games li');
+
+    // Event listener for form submission
+    videoGameForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+      let platform = videoGameForm.elements['platform-family'].value;
+      filterGames(platform);
+    });
+
+    function filterGames(platform) {
+      allGameListItems.forEach(function (item) {
+        if (item.innerText.includes(platform)) {
+          item.classList.remove('hidden-item');
+        } else {
+          item.classList.add('hidden-item');
+        }
+      });
+    }
   });
 
   function renderFavouriteList() {
     let myGamesList = document.querySelector('.my-favourite-games');
     myGamesList.innerHTML = '';
-    
+
     myGames.forEach(game => {
       myGamesList.innerHTML += `<li class="list-group-item">${game}</li>`;
     });
   }
 
-document.addEventListener('DOMContentLoaded', function () {
-  let videoGameForm = document.getElementById('video-game-form');
-  let allGameListItems = document.querySelectorAll('.all-games li');
-
-  // Event listener for form submission
-  videoGameForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    let platform = videoGameForm.elements['platform-family'].value;
-    filterGames(platform);
-  });
-
-  function filterGames(platform) {
-    allGameListItems.forEach(function (item) {
-      if (item.innerText.includes(platform)) {
-        item.classList.remove('hidden-item');
-      } else {
-        item.classList.add('hidden-item');
-      }
-    });
+  function addToFavouriteGame(game) {
+    myGames.push(game);
+    console.log(myGames);
   }
-});
+
+  function removeFromFavouriteGames(game){
+    let index = myGames.indexOf(game);
+    myGames.splice(index, 1);
+    renderFavouriteList();
+  }
 })();
